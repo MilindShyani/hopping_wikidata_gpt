e50a3af90f40ef820657fb40f1d9e5e3af111623
Milind Shyani <mhyani@amazon.com>
2023-07-16 15:25:43 -0700

    Now only get those entities that are also mentioned in the correspondig wikipage

--- wikidata.py
+++ wikidata.py
@@ -1,5 +1,6 @@
 import requests
 import argparse
+from bs4 import BeautifulSoup
 from SPARQLWrapper import SPARQLWrapper, JSON
 
 def get_wikidata_id(entity_name):
@@ -49,6 +50,20 @@ def get_linked_ids(wikidata_id):
     return ids_and_labels
 
 
+def on_wikipedia(name, children_nodes):
+    wiki_url = "https://en.wikipedia.org/wiki/"
+    page_url = wiki_url + name.replace(" ","_")
+    response = requests.get(page_url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    mentioned_entities = []
+    for entity in children_nodes:
+        if soup.body(text=entity[1]):
+            mentioned_entities.append(entity)
+
+    print(f"Removed {-len(mentioned_entities) + len(children_nodes)} entities ")
+    return mentioned_entities
+
 def get_children(qid,entity,depth):
     visited = set()
     to_visit = set()
@@ -58,6 +73,7 @@ def get_children(qid,entity,depth):
     nodes = {}
     nodes[k] = to_visit.copy()
 
+
     while k < depth:
         new_visit = set()
         while to_visit:
@@ -65,6 +81,11 @@ def get_children(qid,entity,depth):
             print("yo")
             if (id, name) not in visited:
                 new_ids_labels = get_linked_ids(id)
+                # new_ids_labels are those child nodes that have a wikipage and are linked to the parent node (id,name)
+                # Only restrict to those child nodes that are mentioned in the parent's wikipage
+
+                new_ids_labels = on_wikipedia(name,new_ids_labels)
+
                 for nid, nlab in new_ids_labels:
                     new_visit.add((nid, nlab))
                 visited.add((id, name))
